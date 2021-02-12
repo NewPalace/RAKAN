4d3edcfdeb56ed97f7ea20af886f403b1c0f8cc4
NewPalace <cristian204@outlook.es>
2021-02-11 19:43:13 -0500

    version 1

---
+++ Geometria.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 11 17:30:47 2021
+
+@author: Rakan
+"""
+### Geometria:
+
+### Links:
+## Link 1
+# Nuckle
+XN1 = 5;
+YN1 = 5;
+ZN1 = 5;
+# Chasis
+XC1 = 5;
+YC1 = 5;
+ZC1 = 5;
+##Link 2
+
+##Link 3
+
+##Link 4
+  
+
+#prueba xd
+A = XN1*XC1;
+print(A, " hola xd")
+
+
+
