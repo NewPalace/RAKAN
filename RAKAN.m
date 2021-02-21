1568a053aed3db0449555dfaddb053cedc8fed1e
NewPalace <cristian204@outlook.es>
2021-02-20 21:03:29 -0500

    Matlab

---
+++ RAKAN.m
@@ -0,0 +1,8 @@
+clear all, close all, clc
+mB = 1;% Vehicle body(sprung mass) mass (Whole car)
+Kw = 1;% Suspension vertical stiffness wheel rate (Total)
+Cw = 1;% Suspension vertical damping coefficient at the wheel (Total)
+mu = 1;% Wheel mass (i.e. unsprung mass) (total)
+Kt = 1;% Tyre vertical stifness (Total)
+Ct = 1;% Tyre vertical damping coefficient (Total, usually small)
+
